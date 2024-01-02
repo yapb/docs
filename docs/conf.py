fea3cdd1757f7c7d979adb7c18339e1a3cc3a53d
Владислав Сухов <22411953+Vladislav4KZ@users.noreply.github.com>
2024-01-02 18:25:30 +0600

    conf.py: use the original Read the Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = [
-    'css/fontstyles.css',
-]
+html_css_files = ['css/fontstyles.css']
+
+# Use the original Read the Docs theme
+extensions = ['sphinx_rtd_theme']
+html_theme = "sphinx_rtd_theme"
